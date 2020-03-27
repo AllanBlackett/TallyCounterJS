4dda7273be2d3c9be0916f915e5c27cd8a6b98d7
AllanLimitless <allanlimitless55@gmail.com>
2020-03-26 20:37:10 -0400

    Updated main.js
    adding more functionality to the timer

--- main.js
+++ main.js
@@ -1,51 +1,80 @@
-let n = 0;
-let body = document.querySelector('body');
+//* The Idea
+// Stopwatch
+// Starts at 0
+// press play to start the timer 
+// you can pause the stopwatch timer and resume
+// you can reset the stopwatch back to 0 
 
 
-//* When up arrow is clicked, increment by 1
-//* When down arrow is clicked, decrement by 1
-//* When spacebar is clicked, value of the counter resets back to zero
 
-// Key press
-body.onkeydown = function (e) {
-  e.preventDefault();
-  // arrow up
-  if (e.keyCode == '38') {
-    document.getElementById("countValue").innerHTML = ++n;
+
+// Timer 
+// placeholder input number 
+// play to start the countdown
+// you can pause and resume 
+// you can clear/restart
+
+  
+// function handler() {
+
+  //taking user input
+  let time1 = document.getElementById('hms').value;
+  //splitting it to seperate variables
+  let pieces = time1.split(":");
+  let hours = pieces[0];
+  let minutes = pieces[1];
+  let seconds = pieces[2];
+  let time = {
+      hours: hours * 1,
+      minutes: minutes * 1,
+      seconds: seconds * 1
+  };
+
+  let myInterval = 0;
+
+  dl.addEventListener("click", function() { 
+
+// Save the interval's handle to `timer`
+let timer = setInterval(countdown, 1000);
+  function countdown() {
+      let container = document.getElementById('count');
+      if (time.seconds > 0) {
+          time.seconds--;
+      } else {
+          if (time.minutes > 0) {
+              time.minutes--;
+              time.seconds = 60;
+          } else {
+              time.minutes = 60;
+              time.seconds = 60;
+              time.hours--;
+          }
+      }
+
+      if (time.hours >= 0) {
+          container.innerHTML = time.hours + ':' + time.minutes + ':' + time.seconds;
+      }
+      else {
+          container.innerHTML = 'Time over';
+          clearInterval(timer);
+      }
+
+      if (document.getElementById('dl').value == 'start')
+      {
+          // Start the timer
+          document.getElementById('sl').value = 'stop';
+          timer = setInterval(countdown, 1000);
+    } else {
+        document.getElementById('dl').value = 'start';
+        clearInterval(timer);
+      }
   }
-  // arrow down
-  else if (e.keyCode == '40') {
-    document.getElementById("countValue").innerHTML = --n;
-  }
-  if (n <= 0) {
-    document.getElementById("countValue").innerHTML = n = 0;
-  }
-  if (e.keyCode == '32') {
-    document.getElementById("countValue").innerHTML = n = 0;
-  }
-};
-
-//.............................................................
-
-//* When the plus icon is clicked, increment by 1
-//* When the minus icon is clicked, decrement by 1
-//* When Reset button is clicked, value of the counter resets back to zero
-
-// plus icon 
-document.getElementById("plus").addEventListener("click", function () {
-  document.getElementById("countValue").innerHTML = ++n;
+  
 });
 
-// minus icon 
-document.getElementById("minus").addEventListener("click", function () {
-  document.getElementById("countValue").innerHTML = --n;
-  // If count is 0, stop decrementing value
-  if (n <= 0) {
-    document.getElementById("countValue").innerHTML = n = 0;
-  }
+sl.addEventListener("click", function() {
+    function myStopFunction() {
+        clearInterval(myVar);
+      } 
 });
 
-// Reset Button (resetButton clicked)
-document.getElementById("resetButton").addEventListener("click", function () {
-  document.getElementById("countValue").innerHTML = n = 0;
-});
